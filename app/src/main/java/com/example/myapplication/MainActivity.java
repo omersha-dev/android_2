9b47180560e52aa311ede61ad1246802415e8d81
omersha-dev <55208673+omersha-dev@users.noreply.github.com>
2022-02-08 21:54:28 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,120 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    MaterialToolbar materialToolbar;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    FrameLayout frameLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        materialToolbar = findViewById(R.id.toolbar);
+        frameLayout = findViewById(R.id.main_frame_layout);
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navigation);
+
+        // Set the custom toolbar as the app's toolbar
+        setSupportActionBar(materialToolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        navigationView.bringToFront();
+
+        // Enable drawer
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                materialToolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close
+        );
+
+        // Add the drawer listener
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
+        // Add the feed fragment
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, new FeedFragment()).commit();
+
+    }
+
+    @Override
+    // Close the drawer when back is clicked if it opened
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        // Manage fragments change
+        switch (item.getItemId()) {
+            case (R.id.feed):
+                fragmentManager.popBackStackImmediate();
+                FeedFragment feedFragment = (FeedFragment) fragmentManager.findFragmentByTag("feed");
+                if (!(feedFragment instanceof FeedFragment)) {
+                    fragmentManager
+                            .beginTransaction()
+                            .replace(R.id.fragment_container, new FeedFragment())
+                            .addToBackStack("feed")
+                            .commit();
+                }
+                break;
+            case (R.id.articles):
+                ArticlesFragment articlesFragment = (ArticlesFragment) fragmentManager.findFragmentByTag("articles");
+                if (!(articlesFragment instanceof ArticlesFragment)) {
+                    fragmentManager
+                            .beginTransaction()
+                            .replace(R.id.fragment_container, new ArticlesFragment())
+                            .addToBackStack("articles")
+                            .commit();
+                }
+                break;
+            case (R.id.new_post):
+                NewPostFragment newPostFragment = (NewPostFragment) fragmentManager.findFragmentByTag("new_post");
+                if (!(newPostFragment instanceof NewPostFragment)) {
+                    fragmentManager
+                            .beginTransaction()
+                            .replace(R.id.fragment_container, new NewPostFragment())
+                            .addToBackStack("new_post")
+                            .commit();
+                }
+                break;
+            case (R.id.signup_nav):
+                SignupFragment signupFragment = (SignupFragment) fragmentManager.findFragmentByTag("signup");
+                if (!(signupFragment instanceof SignupFragment)) {
+                    fragmentManager
+                            .beginTransaction()
+                            .replace(R.id.fragment_container, new SignupFragment())
+                            .addToBackStack("signup")
+                            .commit();
+                }
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return false;
+    }
+
+}
---
+++ app/src/main/java/com/example/myapplication/NewPostFragment.java
@@ -0,0 +1,126 @@
+package com.example.myapplication;
+
+import static android.app.Activity.RESULT_OK;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link NewPostFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class NewPostFragment extends Fragment {
+
+    final int CAMERA_REQUEST = 1;
+    ImageView newPostImageView;
+    Bitmap bitmap;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public NewPostFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment NewPost.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static NewPostFragment newInstance(String param1, String param2) {
+        NewPostFragment fragment = new NewPostFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_new_post, null);
+
+        // Init pets dropdown
+        String[] pets = getResources().getStringArray(R.array.pets);
+        AutoCompleteTextView petsPicker = (AutoCompleteTextView) view.findViewById(R.id.new_post_pet_picker);
+        ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(requireContext(), R.array.pets, R.layout.pet_dropdown_item);
+        petsPicker.setAdapter(arrayAdapter);
+        petsPicker.setThreshold(1);
+
+        // Upload image
+        newPostImageView = view.findViewById(R.id.new_post_image);
+        Button takePictureButton = view.findViewById(R.id.new_post_take_picture);
+        takePictureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, CAMERA_REQUEST);
+            }
+        });
+
+        TextInputEditText textInputEditText = view.findViewById(R.id.new_post_name);
+        Button submitButton = (Button) view.findViewById(R.id.submit_button);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), petsPicker.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        return view;
+//        return inflater.inflate(R.layout.fragment_new_post, container, false);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+            bitmap = (Bitmap) data.getExtras().get("data");
+            newPostImageView.setImageBitmap(bitmap);
+        }
+    }
+}
